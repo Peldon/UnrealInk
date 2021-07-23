c6f39fbe41d388279add954e77e42995341baa0d
Peldon <peldon@quantentunnel.de>
2021-07-23 23:33:46 +0200

    added dependency to ink module in Example project

--- Source/UnrealInkExample/UnrealInkExample.Build.cs
+++ Source/UnrealInkExample/UnrealInkExample.Build.cs
@@ -8,7 +8,7 @@ public class UnrealInkExample : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "Ink" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
